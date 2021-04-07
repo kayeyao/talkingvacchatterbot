7e60903b521c36b14f22d440359172c84505ca96
kayeyao <76432318+kayeyao@users.noreply.github.com>
2021-04-08 02:33:51 +0800

    Add files via upload

--- talkingvacchatterbot_appbody.py
+++ talkingvacchatterbot_appbody.py
@@ -28,6 +28,7 @@ import plotly.express as px
 
 nltk.download('stopwords')
 nltk.download('punkt')
+nltk.download('wordnet')
 
 stats = pd.read_csv('https://covid.ourworldindata.org/data/owid-covid-data.csv')
 stats['date'] = pd.to_datetime(stats['date'])
