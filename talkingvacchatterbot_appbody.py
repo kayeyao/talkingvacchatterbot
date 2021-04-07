ec6b43652da398cb9d8d0ca52919ce4f257b250a
kayeyao <76432318+kayeyao@users.noreply.github.com>
2021-04-08 02:39:37 +0800

    Update talkingvacchatterbot_appbody.py

--- talkingvacchatterbot_appbody.py
+++ talkingvacchatterbot_appbody.py
@@ -401,12 +401,12 @@ vaccinechatbot = ChatBot('VaccineBot', read_only = True,
     trainer='chatterbot.trainers.ListTrainer'
 )
 
-covid_training_data = covidfaqclean
-vaccine_training_data = vaccinefaqclean
+#covid_training_data = covidfaqclean
+#vaccine_training_data = vaccinefaqclean
 covidtrainer = ListTrainer(covidchatbot)
 vaccinetrainer = ListTrainer(vaccinechatbot)
-covidtrainer.train(covid_training_data)
-vaccinetrainer.train(vaccine_training_data)
+#covidtrainer.train(covid_training_data)
+#vaccinetrainer.train(vaccine_training_data)
 
 #covidchatbot.storage.drop()
 #vaccinechatbot.storage.drop()
